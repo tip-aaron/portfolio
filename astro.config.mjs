99b4c6d9beff943324b3fe7eb06a9b91d0e4720c
tip-aaron <qaamragudos@tip.edu.ph>
2025-09-22 15:14:48 +0800

    Integrate autoprefixer into Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,19 @@ import compress from "astro-compress";
 
 import sitemap from "@astrojs/sitemap";
 
+import autoprefixer from "autoprefixer";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://aaronragudos.website",
   integrations: [sitemap(), compress()],
+  vite: {
+      css: {
+          postcss: {
+              plugin: [
+                  autoprefixer()
+              ]
+          }
+      }
+  }
 });
