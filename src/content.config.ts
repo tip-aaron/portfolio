9bf83ac3d594c467d153bf27a7b06a418ba95978
tip-aaron <qaamragudos@tip.edu.ph>
2025-11-17 23:16:46 +0800

    add header links

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,13 @@
-import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "./content/blog/**/*.md" }),
+  loader: glob({ base: "src", pattern: "./content/blog/**/*.md" }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
       description: z.string(),
-      image: image(),
+      image: z.string(),
       author: z.string(),
       publishedOn: z.date(),
       updatedOn: z.date().optional(),
@@ -15,4 +15,31 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+const certifications = defineCollection({
+  loader: glob({ base: "src", pattern: "./content/certifications/**/*.md" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.optional(z.string()),
+      image: z.string(),
+      link: z.string().url(),
+      publishedOn: z.date(),
+      updatedOn: z.date().optional(),
+      draft: z.boolean().optional(),
+    }),
+});
+
+const projects = defineCollection({
+  loader: glob({ base: "src", pattern: "./content/projects/**/*.md" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.optional(z.string()),
+    image: z.string(),
+    link: z.string().url(),
+    publishedOn: z.date(),
+    updatedOn: z.date().optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
+export const collections = { blog, certifications, projects };
---
+++ src/content/certifications/2025-11-17-hackerrank javascript intermediate.md
@@ -0,0 +1,7 @@
+---
+title: Hackerrank JavaScript Intermediate
+description: It covers topics like Design Patterns, Memory management, concurrency model, and event loops, among others.
+image: /uploads/hackerrank-javascript-intermediate.png
+link: https://www.hackerrank.com/certificates/4e015a50e91e
+publishedOn: 2025-11-17
+---
