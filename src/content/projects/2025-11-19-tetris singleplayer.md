110a9c4ed84424f26e4c4517f4f8e147aeda41fd
tip-aaron <qaamragudos@tip.edu.ph>
2025-11-20 02:11:05 +0800

    separate styles and add tetris

---
+++ src/content/projects/2025-11-19-tetris singleplayer.md
@@ -0,0 +1,7 @@
+---
+title: Tetris Singleplayer
+description: A tetris singleplayer built with Astro and PixiJS with settings and custom-made sprite and custom sfx.
+image: /uploads/tetris-singleplayer.png
+link: https://tetris.aaronragudos.website
+publishedOn: 2025-11-19
+---
---
+++ src/lib/masonry-grid.ts
@@ -0,0 +1,55 @@
+import imagesLoaded from "imagesloaded";
+import Masonry from "masonry-layout";
+
+let masonry: Masonry | undefined;
+
+function initMasonry() {
+  const grid = document.getElementById("grid");
+
+  if (!grid) {
+    // This means that we are not on the page
+    // where the masonry grid is present,
+    // since we are using the View
+    // Transitions API. Thus, we
+    // are rerunning this function on
+    // every page load. Refer to the
+    // event listener of "asto:page-load"
+    // below
+    return;
+  }
+
+  masonry = new Masonry(grid, {
+    itemSelector: ".grid-item",
+    columnWidth: ".grid-item",
+    fitWidth: true,
+    gutter: 25,
+    stagger: 0,
+    initLayout: false,
+    transitionDuration: "250ms",
+  });
+
+  if (masonry) {
+    masonry.once?.("layoutComplete", (items: any[]) => {
+      // @ts-ignore
+      document.querySelector(".loader")!.setAttribute("aria-hidden", "true");
+      items.forEach((item) => {
+        item.element.setAttribute("data-finished", "true");
+      });
+    });
+
+    if (masonry) {
+      imagesLoaded(grid, () => {
+        masonry?.layout?.();
+      });
+    }
+  }
+}
+
+document.addEventListener("astro:after-swap", () => {
+  if (masonry) {
+    masonry.destroy?.();
+    masonry = undefined;
+  }
+});
+
+document.addEventListener("astro:page-load", initMasonry);
